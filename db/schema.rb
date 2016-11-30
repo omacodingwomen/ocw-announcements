05a40701ddb4b96a28ad4dbdd0c9a120f7409d2b
Matt Payne <payne@mattpayne.org>
2016-11-30 02:23:26 +0000

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161130022118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "name"
+    t.string   "text"
+    t.string   "twitter_handle"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
